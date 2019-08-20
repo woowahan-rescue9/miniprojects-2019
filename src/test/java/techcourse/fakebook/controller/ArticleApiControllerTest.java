a1e6ec3935fd084f1fea26bc3e3b32bebab7fa74
SunHo Van <vsh123@naver.com>
2019-08-20 13:00:41 +0900

    feat: Article findAll 기능 구현

--- src/test/java/techcourse/fakebook/controller/ArticleApiControllerTest.java
+++ src/test/java/techcourse/fakebook/controller/ArticleApiControllerTest.java
@@ -8,7 +8,10 @@ import techcourse.fakebook.service.dto.ArticleRequest;
 import techcourse.fakebook.service.dto.ArticleResponse;
 import techcourse.fakebook.service.dto.LoginRequest;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ArticleApiControllerTest extends ControllerTestHelper {
@@ -21,6 +24,35 @@ public class ArticleApiControllerTest extends ControllerTestHelper {
     @BeforeEach
     void setUp() {
         cookie = getCookie(login(loginRequest));
+    }
+
+    @Test
+    void 글_목록을_잘_불러오는지_확인한다() {
+        ArticleRequest articleRequest = new ArticleRequest("hello");
+
+        given().
+                port(port).
+                contentType(MediaType.APPLICATION_JSON_UTF8_VALUE).
+                cookie(cookie).
+                body(articleRequest).
+        when().
+                post("/articles").
+        then().
+                statusCode(201).
+                body("content", equalTo(articleRequest.getContent()));
+
+        List<ArticleResponse> articles = given().
+                port(port).
+        when().
+                get("/articles").
+        then().
+                statusCode(200).
+                extract().
+                body().
+                jsonPath().getList(".", ArticleResponse.class);
+
+        assertThat(articles.size()).isGreaterThanOrEqualTo(1);
+
     }
 
     @Test
