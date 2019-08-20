8cf46200fb4580ba81d3ab1be7b533b94b71294f
choihz <choixhz@gmail.com>
2019-08-20 13:38:36 +0900

    refactor: ResponseEntity 리턴시 static factory method 사용

--- src/main/java/techcourse/fakebook/controller/ArticleApiController.java
+++ src/main/java/techcourse/fakebook/controller/ArticleApiController.java
@@ -1,6 +1,5 @@
 package techcourse.fakebook.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import techcourse.fakebook.controller.utils.SessionUser;
@@ -11,6 +10,7 @@ import techcourse.fakebook.service.dto.ArticleResponse;
 import techcourse.fakebook.service.dto.UserOutline;
 
 import java.util.List;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/articles")
@@ -30,31 +30,31 @@ public class ArticleApiController {
     @PostMapping
     public ResponseEntity<ArticleResponse> create(@RequestBody ArticleRequest articleRequest, @SessionUser UserOutline userOutline) {
         ArticleResponse articleResponse = articleService.save(articleRequest, userOutline);
-        return new ResponseEntity<>(articleResponse, HttpStatus.CREATED);
+        return ResponseEntity.created(URI.create("/api/articles")).body(articleResponse);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ArticleResponse> update(@PathVariable Long id, @RequestBody ArticleRequest articleRequest, @SessionUser UserOutline userOutline) {
         ArticleResponse articleResponse = articleService.update(id, articleRequest, userOutline);
-        return new ResponseEntity<>(articleResponse, HttpStatus.OK);
+        return ResponseEntity.ok().body(articleResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ArticleResponse> delete(@PathVariable Long id, @SessionUser UserOutline userOutline) {
         articleService.deleteById(id, userOutline);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}/like")
     public ResponseEntity<ArticleLikeResponse> checkLike(@PathVariable Long id, @SessionUser UserOutline userOutline) {
         ArticleLikeResponse articleLikeResponse = articleService.isLiked(id, userOutline);
-        return new ResponseEntity<>(articleLikeResponse, HttpStatus.OK);
+        return ResponseEntity.ok().body(articleLikeResponse);
     }
 
     @PostMapping("/{id}/like")
     public ResponseEntity<ArticleLikeResponse> like(@PathVariable Long id, @SessionUser UserOutline userOutline) {
         ArticleLikeResponse articleLikeResponse = articleService.like(id, userOutline);
-        return new ResponseEntity<>(articleLikeResponse, HttpStatus.OK);
+        return ResponseEntity.ok().body(articleLikeResponse);
     }
 
     @GetMapping("/{id}/like/count")
