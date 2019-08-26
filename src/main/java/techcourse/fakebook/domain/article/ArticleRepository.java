49ef0dfe14254c6acc34b56144fd464d2ad9c2e1
gimhyogeon <men1219@hanmail.net>
2019-08-26 18:21:57 +0900

    fix: 오타 수정

--- src/main/java/techcourse/fakebook/domain/article/ArticleRepository.java
+++ src/main/java/techcourse/fakebook/domain/article/ArticleRepository.java
@@ -5,7 +5,7 @@ import techcourse.fakebook.domain.user.User;
 
 import java.util.List;
 
-public interface ArticleRepository exgit gtends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findAllByOrderByCreatedDateDesc();
 
     List<Article> findArticlesByUserOrderByCreatedDateDesc(User user);
