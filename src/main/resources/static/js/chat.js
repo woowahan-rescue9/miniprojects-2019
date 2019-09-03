b40f1cf27873c3714d0c05ad95dac9ac22b3a2d6
codeanddonuts <kkw200@naver.com>
2019-09-03 20:36:25 +0900

    refactor: chat.js

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,144 +1,76 @@
-async function connect() {
-    let socket = new SockJS('/websocket');
-    stompClient = Stomp.over(socket);
+import templates from "./templates.js"
+import api from "./api.js"
 
-    stompClient.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
+const WEB_SOCKET_URI = "/websocket"
+const MESSAGE_BROKER_URI = "/api/chatting"
+const REFRESH_INTERVAL = 150
 
-        stompClient.subscribe('/api/chatting', function (messages) {
-            const fromUserId = document.getElementById('user-id').value;
-            const toUserId = document.getElementById('friend-id').value;
-            let json = JSON.parse(messages.body);
-
-            if (json[0].userId == toUserId || json[0].userId == fromUserId) {
-                fetch(BASE_URL + "/api/chats/" + toUserId + "?first=" + false, {
-                    method: "GET",
-                    headers: {
-                        "Accept": "application/json"
-                    }
-                })
-                    .then(response => response.json())
-                    .then(json => {
-                        document.getElementById("message-area").innerHTML = "";
-                        for (let i = 0; i < json.length; i++) {
-                            let pos = !(json[i].userId == fromUserId);
-                            let read = (json[i].read) ? "" : "1";
-                            addMessage(pos, json[i].userName, read, json[i].content);
-                        }
-                    });
-            }
-        });
-    });
-}
-
-const btn = document.getElementById("messenger");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    const fromUserId = document.getElementById('user-id').value;
-    const toUserId = document.getElementById('friend-id').value;
-
-    fetch(BASE_URL + "/api/chats/" + toUserId + "?first=" + true, {
-        method: "GET",
-        headers: {
-            "Accept": "application/json"
+new class ChatService {
+    constructor() {
+        if (document.getElementById("user-id") != null) {
+            this.fromUserId = document.getElementById("user-id").value
+            this.toUserId = document.getElementById("friend-id").value
+            document.getElementById("messenger").addEventListener("click", async () => {
+                this.connect()
+                this.renderMessage(await api.get(`/api/chats/${this.toUserId}?first=false`))
+            })
+            document.getElementById("messenger-close").addEventListener("click", () => this.stompClient.disconnect())
+            document.getElementById("send-message").addEventListener("click", () => {
+                const content = document.getElementById("message-content").value.trim()
+                if (content.length > 0) {
+                    document.getElementById("message-content").value = ""
+                    this.sendMessage(content)
+                    setTimeout(async () => this.renderMessage(await api.get(`/api/chats/${this.toUserId}?first=false`)), REFRESH_INTERVAL)
+                }
+            })
+            window.addEventListener("click", event => {
+                if (event.target == document.getElementById("messenger-modal")) {
+                    this.stompClient.disconnect()
+                }
+            })
         }
-    })
-        .then(response => response.json())
-        .then(json => {
-            document.getElementById("message-area").innerHTML = "";
-            for (let i = 0; i < json.length; i++) {
-                let pos = !(json[i].userId == fromUserId);
-                let read = (json[i].read) ? "" : "1";
-                addMessage(pos, json[i].userName, read, json[i].content);
-            }
-        });
-    connect();
-}
-
-const closeBtn = document.getElementById("messenger-close");
-
-// When the user clicks on <span> (x), close the modal
-closeBtn.onclick = function () {
-    stompClient.disconnect();
-}
-
-window.onclick = function (event) {
-    if (event.target == document.getElementById("messenger-modal")) {
-        stompClient.disconnect();
-    }
-}
-
-const sendMessage = function sendMessage() {
-    document.getElementById("send-message").addEventListener("click", function () {
-        const fromUserId = document.getElementById('user-id').value;
-        const toUserId = document.getElementById('friend-id').value;
-
-        const content = document.getElementById("message-content").value;
-        if (content.length != 0) {
-            document.getElementById("message-content").value = "";
-
-            const chatRequest = {
-                userId: toUserId,
-                content: content
-            };
-
-            fetch(BASE_URL + "/api/chats", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8"
-                },
-                body: JSON.stringify(chatRequest)
-            }).then(response => {
-                fetch(BASE_URL + "/api/chats/" + toUserId + "?first=" + true, {
-                    method: "GET",
-                    headers: {
-                        "Accept": "application/json"
-                    }
-                })
-                    .then(response => response.json())
-                    .then(json => {
-                        document.getElementById("message-area").innerHTML = "";
-                        for (let i = 0; i < json.length; i++) {
-                            let pos = !(json[i].userId == fromUserId);
-                            let read = (json[i].read) ? "" : "1";
-                            addMessage(pos, json[i].userName, read, json[i].content);
-                        }
-                    });
-            }).then(response => {
-                setTimeout(function () {
-                    fetch(BASE_URL + "/api/chats/" + toUserId + "?first=" + true, {
-                        method: "GET",
-                        headers: {
-                            "Accept": "application/json"
-                        }
-                    })
-                        .then(response => response.json())
-                        .then(json => {
-                            document.getElementById("message-area").innerHTML = "";
-                            for (let i = 0; i < json.length; i++) {
-                                let pos = !(json[i].userId == fromUserId);
-                                let read = (json[i].read) ? "" : "1";
-                                addMessage(pos, json[i].userName, read, json[i].content);
-                            }
-                        });
-                }, 100);
-            });
-        }
-    })
-}();
-
-function addMessage(type, name, read, msg) {
-    let message;
-
-    if (type) {
-        const imageSrc = document.getElementById('profileimg').src;
-        message = templates.yourMessage(name, imageSrc, read, msg);
-    } else {
-        message = templates.myMessage(read, msg);
     }
 
-    const messageArea = document.getElementById("message-area");
-    messageArea.insertAdjacentHTML("beforeend", message);
-    messageArea.scrollTop = messageArea.scrollHeight;
-}
+    connect() {
+        this.stompClient = Stomp.over(new SockJS(WEB_SOCKET_URI))
+        this.stompClient.debug = () => {}
+        this.stompClient.connect({}, frame => {
+            this.stompClient.subscribe(MESSAGE_BROKER_URI, async message => {
+                const res = JSON.parse(message.body)
+                if (res[0].userId.toString() === this.fromUserId || res[0].userId.toString() === this.toUserId) {
+                    this.renderMessage(await api.get(`/api/chats/${this.toUserId}?first=false`))
+                }
+            })
+        })
+    }
+
+    async sendMessage(content) {
+        await api.post("/api/chats", {
+            "userId": this.toUserId,
+            "content": content
+        })
+        this.renderMessage(await api.get(`/api/chats/${this.toUserId}?first=true`))
+    }
+
+    renderMessage(messages) {
+        document.getElementById("message-area").innerHTML = ""
+        for (let i = 0; i < messages.length; i++) {
+            const pos = !(messages[i].userId.toString() === this.fromUserId)
+            const isRead = (messages[i].read) ? "" : "1"
+            this.addMessage(pos, messages[i].userName, isRead, messages[i].content)
+        }
+    }
+
+    addMessage(type, name, isRead, content) {
+        const message = (() => {
+            if (type) {
+                return templates.yourMessage(name, document.getElementById("profileimg").src, content)
+            } else {
+                return templates.myMessage(isRead, content)
+            }
+        })()
+        const messageArea = document.getElementById("message-area")
+        messageArea.insertAdjacentHTML("beforeend", message)
+        messageArea.scrollTop = messageArea.scrollHeight
+    }
+}()
